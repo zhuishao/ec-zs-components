a3a9cee6a7bf07d9ac9f98851157ed69c8f5fb30
朱哲伟 <zhewei.zhu@ecarx.com.cn>
2020-11-23 09:24:21 +0800

    feat(locale)插件库国际化尝试

--- packages/index.js
+++ packages/index.js
@@ -1,6 +1,6 @@
 import ZsSearchBox from './ZsSearchBox/index'
 import ParamsInput from './ParamsInput/index'
-// import locale from 'ec-zs-components/src/locale';
+import locale from 'ec-zs-components/src/locale';
 
 const components = [
     ZsSearchBox,
@@ -8,8 +8,8 @@ const components = [
 ]
 
 const install = function(Vue, opts = {}) {
-    // locale.use(opts.locale);
-    // locale.i18n(opts.i18n);
+    locale.use(opts.locale);
+    locale.i18n(opts.i18n);
     components.map(component => {
         Vue.component(component.name, component);
     })
@@ -20,8 +20,8 @@ if(typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-    // locale: locale.use,
-    // i18n: locale.i18n,
+    locale: locale.use,
+    i18n: locale.i18n,
     install,
     ZsSearchBox,
     ParamsInput,
